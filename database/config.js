ee9045b82527bed2bd94bcac6bee961b183c35b9
cristianalvarezz <alvarescristian46@gmail.com>
2021-08-19 17:18:54 -0500

    esto es una prueba

--- database/config.js
+++ database/config.js
@@ -6,13 +6,13 @@ const dbConnection = async() => {
 
     try {
 
-        await mongoose.connect( process.env.MONGODB_CNN, {
+        await mongoose.connect('mongodb+srv://christian:MD8io0JIY4EuLkBq@cluster0.k96kk.mongodb.net/socialcar?authSource=admin&replicaSet=atlas-11008r-shard-0&readPreference=primary&appname=MongoDB%20Compass&ssl=true', {
             useNewUrlParser: true,
             useUnifiedTopology: true,
             useCreateIndex: true,
             useFindAndModify: false
         });
-    
+
         console.log('Base de datos online');
 
     } catch (error) {
