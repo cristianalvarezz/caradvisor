c4ea0ad86584b38f36b6d20dd967154c82f75597
cristianalvarezz <alvarescristian46@gmail.com>
2021-08-19 14:36:13 -0500

    esto es una prueba

--- app.js
+++ app.js
@@ -4,5 +4,12 @@ const Server = require('./models/server');
 
 const server = new Server();
 
-
+server.listen();
 server.conectarDB();
+
+if (window.console) {
+    console.log("Ejemplo de mensaje o log predeterminado");
+    console.error("Ejemplo de mensaje de error");
+    console.warn("Ejemplo de mensaje de advertencia");
+    console.info("Ejemplo de mensaje de información");
+}
